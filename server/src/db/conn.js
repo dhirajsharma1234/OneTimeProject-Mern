8b376c57cc157a68fdbf68e1612fd786d7287459
dhirajsharma1234 <dhirajsharma20042001@gmail.com>
2023-08-30 05:08:55 -0700

    test

--- server/src/db/conn.js
+++ server/src/db/conn.js
@@ -2,8 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async() =>{
     try {
-        // const DB_URL = process.env.DB_URL;
-        const DB_URL = "mongodb+srv://dhiraj:dhiraj@cluster0.ehhvfmx.mongodb.net/oneTime?retryWrites=true&w=majority";
+        const DB_URL = process.env.DB_URL;
         const conn = await mongoose.connect(DB_URL);
 
         if(conn) {
--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,6 @@
 // import 'dotenv/config.js';
-// import dotenv from "dotenv";
-// dotenv.config();
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
@@ -8,8 +8,7 @@ import {router} from "./router/route.js";
 import connectDB from "./db/conn.js";
 const app = express();
 
-const PORT = 8000;
-// const PORT = process.env.PORT || 8001;
+const PORT = process.env.PORT || 8001;
 
 // Configure CORS to allow requests from a specific domain (replace with your frontend domain)
 const corsOptions = {
